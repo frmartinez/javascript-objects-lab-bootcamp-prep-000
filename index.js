ad8364de30957ec6b20c76ab16d9d299b1cb0faa
Fernando Martinez <martinezfer@gmail.com>
2019-01-24 20:34:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  Object.assign({}, object,{[key]: value});
+  Object[key] = Value;
+  return Object
 }
